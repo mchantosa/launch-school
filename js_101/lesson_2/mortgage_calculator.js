e688048cac373988314c02887bc61d7890d3cb2f
Megan Chantosa <meganemmamoore@gmail.com>
2021-08-08 11:38:22 -0700

    made updates to conform to linting rules

--- js_101/lesson_2/mortgage_calculator.js
+++ js_101/lesson_2/mortgage_calculator.js
@@ -19,12 +19,12 @@ EXECUTION:
 //Comment out conductMortgageAnalysis() to run tests quickly
 conductMortgageAnalysis();
 
-function conductMortgageAnalysis(){
+function conductMortgageAnalysis() {
   let doAgain;
   do {
     doAUserDefinedMortgageCalculation();
     doAgain = readline.keyInSelect(['yes', 'no'],"Would you like to run another calculation?");
-  } while(doAgain === 0)
+  } while (doAgain === 0);
   //Good luck hunting
   console.log("Happy house hunting");
 }
@@ -38,13 +38,16 @@ function doAUserDefinedMortgageCalculation() {
   const LOAN_AMOUNT = askUserForLoanAmount();   //float
   const LOAN_DURATION = askUserForLoanDuration();   //{years: int, months: int}
   const MONTHLY_INTEREST_RATE = APR / 1200;   //float
-  const LOAN_DURATION_IN_MONTHS = (LOAN_DURATION.years * 12) + LOAN_DURATION.months;    //int
+  const LOAN_DURATION_IN_MONTHS = (LOAN_DURATION.years * 12) +
+    LOAN_DURATION.months;    //int
 
   //Generate output content
-  const MONTHLY_PAYMENT = findMonthlyPayment(LOAN_AMOUNT, MONTHLY_INTEREST_RATE, LOAN_DURATION_IN_MONTHS);   //float
+  const MONTHLY_PAYMENT = findMonthlyPayment(
+    LOAN_AMOUNT, MONTHLY_INTEREST_RATE, LOAN_DURATION_IN_MONTHS);   //float
   const FIRST_MONTH_INTEREST = LOAN_AMOUNT * MONTHLY_INTEREST_RATE;   //float
   const FIRST_MONTH_PRINCIPLE = MONTHLY_PAYMENT - FIRST_MONTH_INTEREST;   //float
-  const TOTAL_INTEREST = (MONTHLY_PAYMENT * LOAN_DURATION_IN_MONTHS) - LOAN_AMOUNT;   //float
+  const TOTAL_INTEREST =
+    (MONTHLY_PAYMENT * LOAN_DURATION_IN_MONTHS) - LOAN_AMOUNT;   //float
 
   //Return inquiry results to user
   console.log(`For a home loan of the amount $${makeMoneyPretty(LOAN_AMOUNT)} with an APR of ${APR} over a period of ${LOAN_DURATION.years} years and ${LOAN_DURATION.months} months: 
@@ -80,8 +83,12 @@ function askUserForLoanAmount() {
 }
 
 function askUserForLoanDuration() {
+  return {years: askUserForLoanDurationYears(),
+    months: askUserForLoanDurationMonths()};
+}
+
+function askUserForLoanDurationYears() {
   let validLoanYears = false;
-  let validLoanMonths = false;
   const MAX_YEARS = 30;
   while (!validLoanYears && validLoanYears !== 0) {
     validLoanYears = readline.questionInt("What is the duration of your loan in years? (Please give answer as an integer e.g. for 20 years and 6 months enter 20)\n=> ");
@@ -93,6 +100,11 @@ function askUserForLoanDuration() {
       console.log(`The longest term we can submit is ${MAX_YEARS} years, please enter a value less than or equal to ${MAX_YEARS}`);
     }
   }
+  return validLoanYears;
+}
+
+function askUserForLoanDurationMonths() {
+  let validLoanMonths = false;
   while (!validLoanMonths && validLoanMonths !== 0) {
     validLoanMonths = readline.questionInt("What is the 'tail' duration of your loan in months? (Please give answer as an integer e.g. for 20 years and 6 months enter 6, for 30 years enter 0)\n=> ");
     if (validLoanMonths < 0 || validLoanMonths > 11) {
@@ -100,11 +112,13 @@ function askUserForLoanDuration() {
       console.log("You should double check your input, the 'tail end' duration of your loan must be a non negative number no greater than 11");
     }
   }
-  return {years: validLoanYears, months: validLoanMonths};
+  return validLoanMonths;
 }
 
-function findMonthlyPayment(loanAmount, monthlyInterestRate, loanDurationInMonths) {
-  return (loanAmount * monthlyInterestRate) / (1 - Math.pow((1 + monthlyInterestRate),(-loanDurationInMonths)));
+function findMonthlyPayment(
+  loanAmount, monthlyInterestRate, loanDurationInMonths) {
+  return (loanAmount * monthlyInterestRate) /
+  (1 - Math.pow((1 + monthlyInterestRate),(-loanDurationInMonths)));
 }
 
 function makeMoneyPretty(money) {
